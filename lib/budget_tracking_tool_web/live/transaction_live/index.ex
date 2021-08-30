1f1aa159b128fbcaec71fb94d7c73281fb3fd52d
Bart De Caluwe <de.caluwe.bart@gmail.com>
2021-08-30 09:27:32 +0000

    Add: empty state to transactions

--- lib/budget_tracking_tool_web/live/transaction_live/index.ex
+++ lib/budget_tracking_tool_web/live/transaction_live/index.ex
@@ -6,7 +6,9 @@ defmodule BudgetTrackingToolWeb.TransactionLive.Index do
   alias BudgetTrackingTool.Categories
 
   @impl true
-  def mount(_params, _session, socket) do
+  def mount(_params, session, socket) do
+    put_org_id_from_session(session)
+
     {:ok,
      socket
      |> assign(:transactions, list_transactions())
