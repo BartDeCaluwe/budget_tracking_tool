fc72530637a56d947144305a272ee632a8ddd320
Bart De Caluwe <de.caluwe.bart@gmail.com>
2021-08-30 13:10:14 +0000

    Add: default aliases

--- .iex.exs
+++ .iex.exs
@@ -1,1 +1,6 @@
-
+alias BudgetTrackingTool.Repo
+alias BudgetTrackingTool.Transactions.Transaction
+alias BudgetTrackingTool.Categories.Category
+alias BudgetTrackingTool.Books.Book
+alias BudgetTrackingTool.Budgets.Budget
+alias BudgetTrackingTool.Accounts.{User, Org, UserOrg}
