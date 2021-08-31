c352b2232d2b3490c631e86cc203b7a8eabf5f6e
Bart De Caluwe <de.caluwe.bart@gmail.com>
2021-08-31 14:37:32 +0000

    Rename

--- test/budget_tracking_tool/live/page_live_test.exs
+++ test/budget_tracking_tool/live/page_live_test.exs
@@ -5,10 +5,6 @@ defmodule BudgetTrackingToolWeb.PageLiveTest do
 
   test "disconnected and connected render", %{conn: conn} do
     conn = get(conn, "/")
-    disconnected_html = html_response(conn, 200)
-    {:ok, _live, connected_html} = live(conn)
-
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert connected_html =~ "Welcome to Phoenix!"
+    html_response(conn, 302)
   end
 end
