a713028a36ed4dbbe94e684c6ee198a1573d8b05
Bart De Caluwe <de.caluwe.bart@gmail.com>
2021-09-01 09:26:48 +0000

    Add: default noreply address

--- lib/budget_tracking_tool/accounts/user_notifier.ex
+++ lib/budget_tracking_tool/accounts/user_notifier.ex
@@ -9,7 +9,7 @@ defmodule BudgetTrackingTool.Accounts.UserNotifier do
   def deliver_confirmation_instructions(user, url) do
     new()
     |> to(user.email)
-    |> from("info@example.com")
+    |> from(Mailer.get_noreply_address())
     |> render_body("welcome.html", %{user: user, url: url})
     |> Mailer.deliver!()
   end
@@ -20,7 +20,7 @@ defmodule BudgetTrackingTool.Accounts.UserNotifier do
   def deliver_reset_password_instructions(user, url) do
     new()
     |> to(user.email)
-    |> from("info@example.com")
+    |> from(Mailer.get_noreply_address())
     |> render_body("reset_password.html", %{user: user, url: url})
     |> Mailer.deliver!()
   end
@@ -31,7 +31,7 @@ defmodule BudgetTrackingTool.Accounts.UserNotifier do
   def deliver_update_email_instructions(user, url) do
     new()
     |> to(user.email)
-    |> from("info@example.com")
+    |> from(Mailer.get_noreply_address())
     |> render_body("update_email.html", %{user: user, url: url})
     |> Mailer.deliver!()
   end
--- lib/budget_tracking_tool/mailer.ex
+++ lib/budget_tracking_tool/mailer.ex
@@ -1,3 +1,7 @@
 defmodule BudgetTrackingTool.Mailer do
   use Swoosh.Mailer, otp_app: :budget_tracking_tool
+
+  @noreply_address "noreply@budgettrackingtool.com"
+
+  def get_noreply_address, do: @noreply_address
 end
