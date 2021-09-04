db631801be255a4e1fec395ce3f3549aad1fe030
Bart De Caluwe <de.caluwe.bart@gmail.com>
2021-09-04 13:40:07 +0000

    Remove commented lines

--- config/config.exs
+++ config/config.exs
@@ -38,9 +38,6 @@ config :budget_tracking_tool, BudgetTrackingTool.Mailer,
   adapter: Swoosh.Adapters.Sendgrid,
   api_key: "api-key"
 
-# Swoosh API client is needed for adapters other than SMTP.
-# config :swoosh, :api_client, false
-
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
