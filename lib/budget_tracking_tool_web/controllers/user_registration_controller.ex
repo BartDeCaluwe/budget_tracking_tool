ae0112a434b85188589cb197e1167778e51a5553
Bart De Caluwe <de.caluwe.bart@gmail.com>
2021-10-01 09:54:47 +0000

    Fix?: user registration being broken for some reason

--- lib/budget_tracking_tool_web/controllers/user_registration_controller.ex
+++ lib/budget_tracking_tool_web/controllers/user_registration_controller.ex
@@ -15,7 +15,7 @@ defmodule BudgetTrackingToolWeb.UserRegistrationController do
   def create(conn, %{"user" => user_params}) do
     case Accounts.register_user(user_params) do
       {:ok, user} ->
-        {:ok, _} =
+        %{id: _} =
           Accounts.deliver_user_confirmation_instructions(
             user,
             &Routes.user_confirmation_url(conn, :confirm, &1)
